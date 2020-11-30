dd3fb72a79dff4359dcbea8a2ecb4572881d66f2
StevenCreates <steven@stevencreates.tech>
2020-11-29 19:57:23 -0700

    imported new components

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,18 @@ import './App.css';
 import Types from './components/Types'
 import Functions from './components/Functions';
 import { Head } from './components/Head';
+import BasicButton from './components/BasicButton';
+import Header from './components/Header';
+
 
 function App() {
   return (
     <div className="App">
+      <Header/>
         <Head title="Hello" isActive={true}/>
         <Types /> 
         <Functions />
+        <BasicButton />
     </div>
   );
 }
